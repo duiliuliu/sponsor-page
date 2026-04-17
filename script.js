4171e779e26ef5d275000bb6423f7bcf5dbbed10
duiliuliu <pengrui55555@163.com>
2026-04-17 04:07:07 +0000

    feat: 优化代码，支持自适应嵌入
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const qqr = "images/QQPayQR.png";
 const aqr = "images/AliPayQR.jpg";
 const wqr = "images/WeChatQR.jpg";
 
+const isIframe = () => {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+};
+
 const showQR = (QR) => {
   if (QR) MainBox.style.backgroundImage = `url(${QR})`;
 
@@ -16,12 +24,29 @@ const showQR = (QR) => {
   MainBox.classList.add("showQR");
 };
 
+const openQRInNewWindow = (type) => {
+  const baseUrl = window.location.origin + window.location.pathname;
+  window.open(`${baseUrl}?qr=${type}`, "_blank", "width=400,height=500,scrollbars=no,resizable=yes");
+};
+
 donateBox.addEventListener("click", (e) => {
   let el = e.target;
 
   if (el.id === "QQPay") window.open("https://ko-fi.com/E1E81XKC5D", "_blank");
-  else if (el.id === "AliPay") showQR(aqr);
-  else if (el.id === "WeChat") showQR(wqr);
+  else if (el.id === "AliPay") {
+    if (isIframe()) {
+      openQRInNewWindow("alipay");
+    } else {
+      showQR(aqr);
+    }
+  }
+  else if (el.id === "WeChat") {
+    if (isIframe()) {
+      openQRInNewWindow("wechat");
+    } else {
+      showQR(wqr);
+    }
+  }
 });
 
 MainBox.addEventListener("click", () => {
@@ -34,3 +59,18 @@ MainBox.addEventListener("click", () => {
     bd.classList.remove("blur");
   }, 600);
 });
+
+const getUrlParameter = (name) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+};
+
+const qrType = getUrlParameter("qr");
+if (qrType) {
+  bd.classList.add("qr-window");
+  if (qrType === "alipay") {
+    showQR(aqr);
+  } else if (qrType === "wechat") {
+    showQR(wqr);
+  }
+}
